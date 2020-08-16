86542e4076dd23f98a9a5931ffe91f97f7de3f99
Miguel Felix <mikeyfelix99@gmail.com>
2020-08-16 12:59:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
     
   end
   def self.find_or_create_by_name(new_name)
-    self.find_by_name(song_name) || self.create_by_name(song_name)
+    self.find_by_name(new_name) || self.create_by_name(new_name)
   end
 
 end
